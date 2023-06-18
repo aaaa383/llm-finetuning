0e0dd79dd77f4d36e7e23e69f762bb7d88a17ff8
aaaa383 <311.1999.kenta@gmail.com>
2023-06-19 00:38:57 +0900

    fix

--- src/ui/pages/review.py
+++ src/ui/pages/review.py
@@ -7,61 +7,65 @@ import streamlit as st
 # UIのセットアップ
 st.title('OptiMathPro(レビュー画面)')
 
-if "name" not in st.session_state:
-    st.session_state.name = ""
-if "review" not in st.session_state:
-    st.session_state.review = ""
-if "data" not in st.session_state:
-    st.session_state.data = {}
-if "selected_uuid" not in st.session_state:
-    st.session_state.selected_uuid = ""
-
 # 名前入力
-st.session_state.name = st.text_input("ユーザー名を入力してください", value=st.session_state.name)
+name = st.text_input("ユーザー名を入力してください")
 
-if st.session_state.name:
+if name:
     # GETリクエストでデータを取得
     api_endpoint = 'https://wtm35xxg9l.execute-api.ap-northeast-1.amazonaws.com/test/chat'  
-    response = requests.get(api_endpoint, params={'name': st.session_state.name})
+    response = requests.get(api_endpoint, params={'name': name})
 
     if response.status_code == 200:
         # レスポンスを取得し、表示します
-        st.session_state.data = json.loads(response.text)
-        df = pd.DataFrame(st.session_state.data)  # Convert the JSON to pandas DataFrame
+        data_ = json.loads(response.text)
+        df = pd.DataFrame(data_)  # Convert the JSON to pandas DataFrame
         if len(df) == 0:
             st.write("該当するデータが存在しません。")
         else:
-            df = df[['uuid', 'name', 'userInput','response', 'fix_response']]  # Select only the required columns
+            df = df[
+                [
+                    'uuid', 
+                    'name', 
+                    'userInput',
+                    'response', 
+                    'fix_response'
+                ]
+                ].rename(
+                    columns={
+                        'name': 'ユーザー名',
+                        'userInput': '入力文',
+                        'response': '出力文',
+                        'fix_response': '修正内容'
+                    }
+                )
             st.table(df)  # Display the DataFrame as a table
 
-            # If there are any uuids and a uuid hasn't been selected yet, select the first one
-            if len(df['uuid'].tolist()) > 0 and st.session_state.selected_uuid == "":
-                st.session_state.selected_uuid = df['uuid'].tolist()[0]
+            # If there are any uuids, select the first one by default
+            if len(df['uuid'].tolist()) > 0:
+                selected_uuid = df['uuid'].tolist()[0]
 
-            # Create a select box with the uuids
-            uuid = st.selectbox("UUIDを選択してください", options=df['uuid'].tolist(), key='selected_uuid')
+                # Create a select box with the uuids
+                uuid = st.selectbox("UUIDを選択してください", options=df['uuid'].tolist(), index=0)
 
-            # Display the selected row
-            selected_row = df[df['uuid'] == st.session_state.selected_uuid]
-            st.write(selected_row)
+                # Display the selected row
+                selected_row = df[df['uuid'] == uuid]
+                st.write(selected_row)
 
-            # Review text box
-            st.session_state.review = st.text_input("responseの修正内容を記入してください", value=st.session_state.review)
+                # Review text box
+                review = st.text_input("responseの修正内容を記入してください")
 
-            # Submission button
-            if st.button("提出"):
-                put_url = api_endpoint + '/' + uuid
-                body = {'uuid': uuid, 'fix_response': str(st.session_state.review)}
+                # Submission button
+                if st.button("提出"):
+                    put_url = api_endpoint + '/' + uuid
+                    body = {'fix_response': str(review)}
 
-                # リクエストの送信
-                response = requests.put(put_url, data=json.dumps(body))
-
-                print('response',response)
-
-                if response.status_code == 200:
+                    # リクエストの送信
+                    response = requests.put(put_url, data=json.dumps(body))
                     st.success("修正内容が提出されました。")
-                else:
-                    st.exception("修正内容の反映に失敗しました。")
+                    # if response.status_code == 200:
+                    #     st.success("修正内容が提出されました。")
+                    # else:
+                    #     st.exception("修正内容の反映に失敗しました。")
            
     else:
         st.write("Error: ", response.status_code)
